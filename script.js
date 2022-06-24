60a40b0164697cf57af8450b7be52648b844e23c
archolisa <105179700+archolisa@users.noreply.github.com>
2022-06-24 03:48:00 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const myName = "Nwankwo Akaolisa John";
+const myHeight = "190 cm";
+const myCountry = "Nigeria"
+
+const paragraph  = (` Name: ${myName}.\n Height: ${myHeight}.\n Country: ${myCountry}.`)
+ alert(paragraph)
